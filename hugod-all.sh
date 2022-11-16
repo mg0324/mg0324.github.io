830cb359eb7eac58d135b39368fdf2d19f7879f3
mango mac <1092017732@qq.com>
2022-11-16 22:42:45 +0800

    迁移到hw

--- hugod-all.sh
+++ hugod-all.sh
@@ -4,7 +4,7 @@ hugo --gc --minify
 rm -rf public/images
 # scp -r public/img/* root@node:/data/img
 rm -rf public/img
-scp -r public/* root@node:/data/mb
+scp -r public/* root@hw:/data/mb
 echo ‘上传到node服务器成功’
 rm -rf public
 rm -rf mb
