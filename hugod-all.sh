6d9b61592c3b517ac8705347744347bf1b441f95
mango mac <1092017732@qq.com>
2022-11-16 23:13:19 +0800

    发布

--- hugod-all.sh
+++ hugod-all.sh
@@ -1,10 +1,10 @@
 rm -rf public
 hugo --gc --minify
-scp -r public/images/* root@node:/data/mb/images/
+scp -r public/images/* root@hw:/data/mb/images
 rm -rf public/images
-scp -r public/img/* root@node:/data/mb/img/
+scp -r public/img/* root@hw:/data/mb/img
 rm -rf public/img
-scp -r public/* root@hw:/data/mb/
+scp -r public/* root@hw:/data/mb/mb
 echo ‘上传到node服务器成功’
 rm -rf public
 rm -rf mb
